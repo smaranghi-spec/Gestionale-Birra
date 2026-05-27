21e6b60806935f0d35f901b03fc2dd95e22bf5ba
smaranghi-spec <s.maranghi@gmail.com>
2026-05-27 10:31:35 +0200

    main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Gestionale Birrificio")
 
-@app.get("/")
-def home():
-    return {"status": "ok", "app": "Gestionale Birrificio"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "ricette.html",
+        {
+            "request": request,
+            "ricette": []
+        }
+    )
+
+
+@app.get("/ricette/html", response_class=HTMLResponse)
+def elenco_ricette(request: Request):
+    return templates.TemplateResponse(
+        "ricette.html",
+        {
+            "request": request,
+            "ricette": []
+        }
+    )
+
+
+@app.get("/catalogo-ingredienti/html", response_class=HTMLResponse)
+def catalogo_ingredienti(request: Request):
+    return templates.TemplateResponse(
+        "catalogo_ingredienti.html",
+        {
+            "request": request,
+            "ingredienti": []
+        }
+    )
