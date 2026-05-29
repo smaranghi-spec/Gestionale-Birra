b903fbed2424389b57a72263a29ae9e3f1a1bd12
smaranghi-spec <s.maranghi@gmail.com>
2026-05-29 18:25:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from .db import engine
@@ -40,4 +40,4 @@ app.include_router(acquisti.router)
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {})
+    return templates.TemplateResponse("home.html", {"request": request})
