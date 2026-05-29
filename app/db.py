cb70853818abb393d866f223c9f3349848f8ad12
smaranghi-spec <s.maranghi@gmail.com>
2026-05-29 19:44:14 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./gestionale_birra.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 Base = declarative_base()
