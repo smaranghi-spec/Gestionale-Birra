3c60091f5f5118ac970bff79abb8670818782bde
smaranghi-spec <s.maranghi@gmail.com>
2026-05-29 17:53:15 +0200

    Create acquisti.py

---
+++ app/routers/acquisti.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/acquisti", response_class=HTMLResponse)
+def pagina_acquisti(request: Request):
+    return templates.TemplateResponse(request, "acquisti.html", {})
